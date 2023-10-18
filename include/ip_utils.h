42d410d9f7ac6d37cc6180748fd5b26f394ea10a
Liliya Alyoshina <alyoshina.li@gmail.com>
2023-10-18 13:47:27 +0300

    print ip

--- include/ip_utils.h
+++ include/ip_utils.h
@@ -1,23 +1,95 @@
 #pragma once
 
-#include <cassert>
-#include <cstdlib>
+#include <type_traits>
+#include <algorithm>
 #include <iostream>
-#include <string>
 #include <vector>
-#include <array>
-#include <functional>
+#include <list>
 
-namespace IpUtils {
+namespace ip_utils {
 
-using IpType = std::array<std::uint8_t, 4>;
-using IpPoolType = std::vector<IpType>;
+namespace details {
 
-const std::uint16_t NOT_IP_NUMBER = 256;
-void print(IpPoolType& ip_pool);
-std::vector<std::string> split(const std::string &str, char d);
-void sort(IpPoolType& ip_pool);
-IpPoolType filter(IpPoolType& ip_pool, const std::string &str);
-IpPoolType filter(IpPoolType& ip_pool, const std::vector<std::uint8_t>& v);
-IpPoolType filter(IpPoolType& ip_pool, const std::function<bool(const IpType& ip)>lambda);
+template<typename, typename = void>
+struct is_iterable : std::false_type {};
+ 
+template<typename T>
+struct is_iterable<T, std::void_t<decltype(std::declval<T>().begin()),
+                                    decltype(std::declval<T>().end())>> : std::true_type {};
+
+template <typename T>
+constexpr bool is_iterable_v = is_iterable<T>::value;
+
+template<typename... T>
+struct is_tuple : std::false_type {};
+
+template<typename... T>
+struct is_tuple<std::tuple<T...>> : std::true_type {};
+
+template <typename... T>
+constexpr bool is_tuple_v = is_tuple<T...>::value;
+
+} //details
+
+template <typename T>
+auto shift_r(T v, std::size_t n) -> std::enable_if_t<std::is_integral_v<T>, int16_t> {
+    T temp = v >> n;
+    temp &= 255;
+    return int16_t((uint8_t)temp);
 }
+
+template <typename T>
+auto print_ip(T ip, std::ostream& os = std::cout) -> std::enable_if_t<std::is_integral_v<T>> {
+    std::size_t n = sizeof(T);
+    if (n > 0) {
+        os << shift_r(ip, (n - 1) * 8);
+        for (int i = 1; i < n; ++i) {
+            os << "." << shift_r(ip, (n - 1 - i) * 8);
+        }
+    }
+    os << std::endl;
+}
+
+template <typename T>
+auto print_ip(T ip, std::ostream& os = std::cout) -> std::enable_if_t<std::is_same_v<std::string, std::remove_reference_t<T>> 
+                                        || std::is_same_v<char *, std::remove_reference_t<T>> 
+                                        || std::is_same_v<const char *, std::remove_reference_t<T>>> {
+    os << ip << std::endl;
+}
+
+template <typename T>
+auto print_ip(T ip, std::ostream& os = std::cout) -> std::enable_if_t<not std::is_same_v<std::string, std::remove_reference_t<T>>
+                                            && details::is_iterable_v<T>> {
+    if (!ip.empty()) {
+        os << *ip.begin();
+        std::for_each(std::next(ip.begin()), ip.end(), [&os] (auto& value) {
+            os << "." << value;
+        });
+        os << std::endl;
+    }
+}
+
+
+template<std::size_t N = 0, typename... T>
+auto print_tuple(std::tuple<T...>& v, std::ostream& os = std::cout) -> std::enable_if_t<N == sizeof...(T)> {
+    os << std::endl;
+}
+
+template<std::size_t N = 0, typename... T>
+auto print_tuple(std::tuple<T...>& v, std::ostream& os = std::cout) -> std::enable_if_t<N < sizeof...(T)> {
+    if constexpr (N == 0) {
+        os << std::get<N>(v);
+    } else {
+        static_assert(std::is_same_v<decltype(std::get<N - 1>(v)), decltype(std::get<N>(v))>
+                                                        , "std::tuple contains different types");
+        os << "." << std::get<N>(v);
+    }
+    print_tuple<N + 1, T...>(v, os);
+}
+
+template <typename T>
+auto print_ip(T ip, std::ostream& os = std::cout) -> std::enable_if_t<details::is_tuple_v<T>> {
+    print_tuple(ip, os);
+}
+
+} //ip_utils
--- src/main.cpp
+++ src/main.cpp
@@ -1,52 +1,22 @@
 #include "ip_utils.h"
 
-using namespace IpUtils;
+#include <exception>
 
-#include <array>
-#include <algorithm>
-//cat ../ip_filter_simple.tsv | ./ip_filter
+using namespace ip_utils;
+
 int main(int argc, char const *argv[])
 {
     try {
-        IpPoolType ip_pool;
-
-        for(std::string line; std::getline(std::cin, line);) {
-            std::vector<std::string> v = split(line, '\t');
-            std::vector<std::string> ip_str = split(v.at(0), '.');
-            ip_pool.emplace_back(IpType{});
-            std::transform(ip_str.cbegin(), ip_str.cend(), ip_pool.back().begin(), [](auto& val) { return std::atoi(val.c_str()); });   
-        }
-        
-        //reverse lexicographically sort
-        sort(ip_pool);
-        print(ip_pool);
-
-        //filter by first byte and output
-        {
-            //IpPoolType f = filter(ip_pool, std::string("1.*.*.*"));
-            IpPoolType f = filter(ip_pool, [](const IpType& ip ) { return ip[0] == 1; });
-            print(f);
-        }
-        //filter by first byte and output
-        {
-            //IpPoolType f = filter(ip_pool, std::string("46.70.*"));
-            IpPoolType f = filter(ip_pool, [](const IpType& ip ) { return ip[0] == 46 && ip[1] == 70; });
-            print(f);
-        }
-        //filter by any byte and output
-        {
-            //std::vector<std::uint8_t> v{46, 29};
-            //std::vector<std::uint8_t> v{46};
-            //IpPoolType f = filter(ip_pool, v);
-            IpPoolType f = filter(ip_pool, [](const IpType& ip ) {
-                bool ret_val = false;
-                for (auto& ip_n: ip) {
-                    ret_val = ret_val || (ip_n == 46);
-                }
-                return ret_val; 
-            });
-            print(f);
-        }
+        print_ip(int8_t{-1});
+        print_ip(int16_t{0});
+        print_ip(int32_t{2130706433});
+        print_ip(int64_t{8875824491850138409});
+        print_ip(std::string{"Hello, World!"});
+        //print_ip("Hello, World!"); // Hello, World!
+        print_ip(std::vector<int>{100, 200, 300, 400});
+        print_ip(std::list<short>{400, 300, 200, 100});
+        //print_ip(std::vector<int8_t>{-1, 6, 5, -1});
+        print_ip(std::make_tuple(123, 456, 789, 0));
     } catch(const std::exception &e) {
         std::cerr << e.what() << std::endl;
     }
